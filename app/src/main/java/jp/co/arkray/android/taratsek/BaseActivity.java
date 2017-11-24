1a9ae6acad06143c280109053176e270c19726fb
June Jovi Granados <jjgranados.arkray@gmail.com>
2017-11-24 10:27:57 +0800

    initial commit

---
+++ app/src/main/java/jp/co/arkray/android/taratsek/BaseActivity.java
@@ -0,0 +1,8 @@
+package jp.co.arkray.android.taratsek;
+
+/**
+ * Created by USER on 11/24/2017.
+ */
+
+public class BaseActivity {
+}
---
+++ app/src/main/java/jp/co/arkray/android/taratsek/SplashActivity.java
@@ -0,0 +1,13 @@
+package jp.co.arkray.android.taratsek;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
