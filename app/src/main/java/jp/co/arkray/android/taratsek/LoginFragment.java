f81cedb07382404b4b4b6685455c6b9987c71c52
June Jovi Granados <jjgranados.arkray@gmail.com>
2017-12-05 15:57:14 +0800

    STRUCTURE UPDATE

--- app/src/main/java/jp/co/arkray/android/taratsek/LoginFragment.java
+++ app/src/main/java/jp/co/arkray/android/taratsek/LoginFragment.java
@@ -2,6 +2,7 @@ package jp.co.arkray.android.taratsek;
 
 import android.app.Activity;
 import android.content.Context;
+import android.databinding.DataBindingUtil;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -9,7 +10,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class LoginFragment extends Fragment {
+import jp.co.arkray.android.taratsek.databinding.FragmentLoginBinding;
+
+public class LoginFragment extends Fragment implements View.OnClickListener {
+
+    private FragmentLoginBinding mBinding;
 
     public LoginFragment() {
     }
@@ -23,7 +28,25 @@ public class LoginFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        mBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_login,container,false);
+        initialize();
+        return mBinding.getRoot();
     }
 
+    public void initialize(){
+        mBinding.btnLogin.setOnClickListener(this);
+        mBinding.btnRegister.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_login:
+
+                break;
+            case R.id.btn_register:
+
+                break;
+        }
+    }
 }
--- app/src/main/java/jp/co/arkray/android/taratsek/SplashActivity.java
+++ app/src/main/java/jp/co/arkray/android/taratsek/SplashActivity.java
@@ -1,49 +1,48 @@
 package jp.co.arkray.android.taratsek;
 
+import android.animation.ValueAnimator;
 import android.databinding.DataBindingUtil;
 import android.os.Handler;
+import android.support.transition.TransitionManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
 import jp.co.arkray.android.taratsek.Utilities.AnimUtils;
 import jp.co.arkray.android.taratsek.databinding.ActivitySplashBinding;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity{
 
-
-    ActivitySplashBinding mBinding;
+    private ActivitySplashBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mBinding = DataBindingUtil.setContentView(this,R.layout.activity_splash);
+        initialize();
+
+    }
+
+    public void initialize(){
 
         LoginFragment mFragment = new LoginFragment();
-
         FragmentTransaction mTransaction = getSupportFragmentManager().beginTransaction();
         mTransaction.add(R.id.frame_login_container, mFragment);
         mTransaction.commit();
-        //AnimUtils.collapse(mBinding.frameLoginContainer);
-        mBinding.frameLoginContainer.setVisibility(View.INVISIBLE);
-        mBinding.progressSplashLoading.setVisibility(View.VISIBLE);
 
+        mBinding.progressSplashLoading.setVisibility(View.VISIBLE);
+        AnimUtils.collapse(mBinding.frameLoginContainer);
         final Handler handler = new Handler();
         final Runnable r = new Runnable() {
             public void run() {
                 mBinding.progressSplashLoading.setVisibility(View.GONE);
-
-                //AnimUtils.expand(mBinding.frameLoginContainer);
-                //handler.postDelayed(this, 1000);
+                AnimUtils.expand(mBinding.frameLoginContainer);
             }
         };
         handler.postDelayed(r, 5000);
-
-
-
-
-
-
     }
+
 }
