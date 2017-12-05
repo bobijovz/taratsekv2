40e58e78d26473b0ab8fcbe2a803158ac7ff77f5
June Jovi Granados <jjgranados.arkray@gmail.com>
2017-12-05 09:35:56 +0800

    anim utils

--- app/src/main/java/jp/co/arkray/android/taratsek/SplashActivity.java
+++ app/src/main/java/jp/co/arkray/android/taratsek/SplashActivity.java
@@ -1,10 +1,13 @@
 package jp.co.arkray.android.taratsek;
 
 import android.databinding.DataBindingUtil;
+import android.os.Handler;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
+import jp.co.arkray.android.taratsek.Utilities.AnimUtils;
 import jp.co.arkray.android.taratsek.databinding.ActivitySplashBinding;
 
 public class SplashActivity extends AppCompatActivity {
@@ -22,6 +25,24 @@ public class SplashActivity extends AppCompatActivity {
         FragmentTransaction mTransaction = getSupportFragmentManager().beginTransaction();
         mTransaction.add(R.id.frame_login_container, mFragment);
         mTransaction.commit();
+        //AnimUtils.collapse(mBinding.frameLoginContainer);
+        mBinding.frameLoginContainer.setVisibility(View.INVISIBLE);
+        mBinding.progressSplashLoading.setVisibility(View.VISIBLE);
+
+        final Handler handler = new Handler();
+        final Runnable r = new Runnable() {
+            public void run() {
+                mBinding.progressSplashLoading.setVisibility(View.GONE);
+
+                //AnimUtils.expand(mBinding.frameLoginContainer);
+                //handler.postDelayed(this, 1000);
+            }
+        };
+        handler.postDelayed(r, 5000);
+
+
+
+
 
 
     }
---
+++ app/src/main/java/jp/co/arkray/android/taratsek/Utilities/AnimUtils.java
@@ -0,0 +1,77 @@
+package jp.co.arkray.android.taratsek.Utilities;
+
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.Transformation;
+import android.widget.LinearLayout;
+
+/**
+ * Created by USER on 12/5/2017.
+ */
+
+public class AnimUtils {
+
+    public static void expand(final View v) {
+
+        v.measure(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        final int targetHeight = v.getMeasuredHeight();
+
+        v.getLayoutParams().height = 1;
+        v.setVisibility(View.VISIBLE);
+        Animation a = new Animation() {
+            @Override
+            protected void applyTransformation(float interpolatedTime, Transformation t) {
+                v.getLayoutParams().height = interpolatedTime == 1
+                        ? LinearLayout.LayoutParams.WRAP_CONTENT
+                        : (int) (targetHeight * interpolatedTime);
+                v.requestLayout();
+            }
+
+            @Override
+            public boolean willChangeBounds() {
+                return true;
+            }
+        };
+        a.setDuration((int) (targetHeight / v.getContext().getResources().getDisplayMetrics().density));
+        a.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                //binder.scrollview.scrollTo(0, v.getTop());
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        v.startAnimation(a);
+    }
+
+    public static void collapse(final View v) {
+        final int initialHeight = v.getMeasuredHeight();
+
+        Animation a = new Animation() {
+            @Override
+            protected void applyTransformation(float interpolatedTime, Transformation t) {
+                if (interpolatedTime == 1) {
+                    v.setVisibility(View.GONE);
+                } else {
+                    v.getLayoutParams().height = initialHeight - (int) (initialHeight * interpolatedTime);
+                    v.requestLayout();
+                }
+            }
+
+            @Override
+            public boolean willChangeBounds() {
+                return true;
+            }
+        };
+        a.setDuration((int) (initialHeight / v.getContext().getResources().getDisplayMetrics().density));
+        v.startAnimation(a);
+    }
+}
